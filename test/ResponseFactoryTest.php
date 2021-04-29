f2c88cd4a15eea21b1a69e390c24e103d1ab5479
hysryt <hysryt@gmail.com>
2021-04-29 21:52:41 +0900

    add: ResponseFactory実装

---
+++ test/ResponseFactoryTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Hysryt\Bookmark\Test;
+
+use Hysryt\Bookmark\Lib\HttpMessage\ResponseFactory;
+
+require_once(__DIR__ . '/../www/inc/autoload.php');
+
+class ResponseFactoryTest {
+    public function testNormal() {
+        $factory = new ResponseFactory();
+        $response = $factory->createResponse();
+        assert($response->getStatusCode() === 200);
+        assert($response->getReasonPhrase() === '');
+    }
+
+    public function test404() {
+        $factory = new ResponseFactory();
+        $response = $factory->createResponse(404, 'Not found');
+        assert($response->getStatusCode() === 404);
+        assert($response->getReasonPhrase() === 'Not found');
+    }
+}
+
+$test = new ResponseFactoryTest();
+$test->testNormal();
+$test->test404();
